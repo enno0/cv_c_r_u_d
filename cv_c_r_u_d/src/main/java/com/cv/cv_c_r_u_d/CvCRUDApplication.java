d04b70c77dad5bdc8fb0d82a6750d9e3fcc80335
kamalaldeen <kamalaldeenmohamed@gmail.com>
2024-05-21 13:32:35 +0300

    cv_project

---
+++ cv_c_r_u_d/src/main/java/com/cv/cv_c_r_u_d/CvCRUDApplication.java
@@ -0,0 +1,13 @@
+package com.cv.cv_c_r_u_d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvCRUDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvCRUDApplication.class, args);
+	}
+
+}
---
+++ cv_c_r_u_d/src/test/java/com/cv/cv_c_r_u_d/CvCRUDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.cv_c_r_u_d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvCRUDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
